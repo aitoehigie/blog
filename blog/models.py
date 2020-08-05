baa939db0fca85d95754fc27a1a351e7382911b2
ehigie aito <ehigie.aito@platform.sh>
2020-08-05 03:46:42 +0100

    create a base template

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -28,7 +29,6 @@ class Post(models.Model):
         if not self.id:
             self.slug = slugify(self.title)
         super(Post, self).save(*args, **kwargs)
-    
-
-
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 
